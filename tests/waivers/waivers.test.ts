c0b777a600319e12981f8782b3b8750545c4c9d2
Blake Hildebrand <baugusthildebrand@gmail.com>
2018-07-04 13:37:51 -0500

    Add initial waiver test

---
+++ tests/waivers/waivers.test.ts
@@ -0,0 +1,28 @@
+import EspnFantasyFootball from "./../../ts/EspnFantasyFootball";
+import * as request from 'request';
+import * as html from './WaiversHtml';
+import * as results from './WaiverResults'
+
+// var matchupsResult = require('./waivers.json');
+
+const scraper = new EspnFantasyFootball({
+    leagueId: 106980 //Test league I created for this purpose.
+});
+
+jest.mock('request');
+
+test('Verify that matchups are correctly parsed', () => {
+    request.mockImplementation(requestMock);
+
+    scraper.getWaivers(null, (err, players) => {
+        expect(JSON.stringify(players)).toEqual(results.noFilterStr);
+    });
+  });
+
+function requestMock(options: request.Options, callback: request.RequestCallback ) {
+    let response = {
+        statusCode: 200
+    }
+
+    callback(null, response, html.noFilterWaiverHTML);
+}
